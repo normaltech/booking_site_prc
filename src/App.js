826678192569bd3f09ce0e1922f47390e8b3c296
normaltech <silkcarp97@gmail.com>
2022-07-06 11:31:53 +0900

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { List } from "./list/List";
+import { Home } from "./pages/home/Home";
+import { Hotel } from "./pages/hotel/Hotel";
+
 function App() {
   return (
-    <div className="App">
-      hello world!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/hotels" element={<List/>}/>
+        <Route path="/hotels/:id" element={<Hotel/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
